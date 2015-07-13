6e0438ff543c89764d1faf6ce0d3dfa11cfcdadc
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2015-07-13 14:28:02 -0400

    SQL script fixed

--- CREATE-TABLE-incoming.sql
+++ CREATE-TABLE-incoming.sql
@@ -1,12 +1,7 @@
-DROP TABLE IF EXISTS incoming;
+﻿DROP TABLE IF EXISTS incoming;
 
-CREATE TABLE incoming
-	(page 		varchar,
-	 lang 		varchar,
-	 incoming 	integer,
-	 id 		serial primary key )
-AS
-	SELECT "to" AS page, lang, SUM(links.links + links.mentions) AS incoming
+CREATE TABLE incoming AS 
+  SELECT "to" AS page, lang, SUM(links.links + links.mentions) AS incoming
 	FROM links
 	GROUP BY "to", lang;
 

3280fe9d27391cca4efa1d02d6df055edf9a2671
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2015-11-02 21:27:07 -0500

    Minor changes

--- threadedlocationparser.py
+++ threadedlocationparser.py
@@ -2,21 +2,21 @@
 import re                     # Regex
 from lxml import etree        # XML parsing
 import timeit                 # simple benachmarking
-# encoding=utf8  
-import sys  
+# encoding=utf8
+import sys
 
-reload(sys)  
+reload(sys)
 sys.setdefaultencoding('utf8')
 
-import LatLon                 # conversion between degrees, minutes, seconds and decimal degrees 
+import LatLon                 # conversion between degrees, minutes, seconds and decimal degrees
 
 lang = "en"  # so that we can keep track of different languages in our DB
 
 ns = '{http://www.mediawiki.org/xml/export-0.10/}'
 
-# these control how many copies of this script will be started, and which of those 
+# these control how many copies of this script will be started, and which of those
 # this particular instance takes care of. Can be used to only parse every n-th page.
-# These will be read from command line args! 
+# These will be read from command line args!
 global numthreads # arg 1
 global thisthread # arg 2
 global infile     # arg 3 - the wikipedia XML dump
@@ -63,7 +63,7 @@ def addInsert(pagetitle, lat, lon, sqlfile):
     # skip coordinates that are wrong (or on the moon or other planets ...)
     if (lat < -90.0) or (lat > 90.0) or (lon < -180.0) or (lon > 180.0):
         logme("coords out of bounds for " + pagetitle +": " + str(lat) +", " + str(lon))
-        # raise ValueError("Coordinates out of range.")    
+        # raise ValueError("Coordinates out of range.")
         return
 
     insert = u"INSERT INTO pages VALUES ('"+pagetitle.replace("'", "''")+"', '"+lang+"', ST_SetSRID(ST_MakePoint("+str(lon)+", "+str(lat)+"),4326));\n"
@@ -72,8 +72,8 @@ def addInsert(pagetitle, lat, lon, sqlfile):
 # turns strings into floats, returning 0.0 for empty strings
 def replaceEmptyFloat(matchGroup):
     if len(matchGroup.strip()) == 0:
-        return 0.0 
-    else: 
+        return 0.0
+    else:
         return float(matchGroup.strip())
 
 # this is the heart of the script, parsing through the text on a wikipedia page
@@ -84,10 +84,10 @@ def findreferences(page, sqlfile):
 
     pagetitle = page.find(ns+'title').text
     pagetext  = page.find(ns+'revision/'+ns+'text').text
-    
+
     # find all coords via regex
     try:
-        coordmatch = coordpattern.search(pagetext) 
+        coordmatch = coordpattern.search(pagetext)
     except Exception:
         logme("Exception caught during coord search. Empty page text?")
         return
@@ -95,8 +95,8 @@ def findreferences(page, sqlfile):
     #sqlfile.write(pagetitle.encode('utf8')+"\n")
 
     if coordmatch:
-        #sqlfile.write(u" --- coords: " + str(coordmatch.group()) + "\n"); 
-        
+        #sqlfile.write(u" --- coords: " + str(coordmatch.group()) + "\n");
+
         # figure out if the string contains heading directions and if it does,
         # where they are:
         i = 1
@@ -112,23 +112,19 @@ def findreferences(page, sqlfile):
                 longhindex = i
             i = i+1
 
-        #sqlfile.write("lathindex: " + str(lathindex) + ", longhindex: " + str(longhindex) + "\n") 
+        #sqlfile.write("lathindex: " + str(lathindex) + ", longhindex: " + str(longhindex) + "\n")
 
-        # first handle the cases where we don't have headings        
+        # first handle the cases where we don't have headings
         if lathindex == 0 and longhindex == 0:
-            try:
-                addInsert(pagetitle, str(float(coordmatch.group(1))), str(float(coordmatch.group(2))), sqlfile)
-            except Exception:
-                pass
-            return
+            addInsert(pagetitle, str(float(coordmatch.group(1))), str(float(coordmatch.group(2))), sqlfile)
 
         # if we have headings, we have three different cases:
         # 1. just degrees
         if lathindex == 2 and longhindex == 4:  # continue here!
             latstring  = str(coordmatch.group(1)).strip() + " " + str(coordmatch.group(2)).strip()
             longstring = str(coordmatch.group(3)).strip() + " " + str(coordmatch.group(4)).strip()
-            
-            # just to check: 
+
+            # just to check:
             # sqlfile.write(latstring +" "+ longstring+ "\n")
 
             coords = LatLon.string2latlon(latstring, longstring, 'd% %H')
@@ -141,8 +137,8 @@ def findreferences(page, sqlfile):
         if lathindex == 3 and longhindex == 6:  # continue here!
             latstring = str(coordmatch.group(1)).strip() + " " + str(float(coordmatch.group(2))).strip() + " " + str(coordmatch.group(3)).strip()
             longstring = str(coordmatch.group(4)).strip() + " " + str(float(coordmatch.group(5))).strip() + " " + str(coordmatch.group(6)).strip()
-            
-            # just to check: 
+
+            # just to check:
             # sqlfile.write(latstring +" "+ longstring+ "\n")
 
             coords = LatLon.string2latlon(latstring, longstring, 'd% %m% %H')
@@ -151,7 +147,7 @@ def findreferences(page, sqlfile):
             addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
         # 3. degrees + minutes + seconds
         if lathindex == 4 and longhindex == 8:  # continue here!
-            
+
             latstring = str(coordmatch.group(1)).strip() + " " + str(replaceEmptyFloat(coordmatch.group(2))) + " " + str(replaceEmptyFloat(coordmatch.group(3))) + " "+ str(coordmatch.group(4)).strip()
             longstring = str(coordmatch.group(5)).strip() + " " + str(replaceEmptyFloat(coordmatch.group(6))) + " " + str(replaceEmptyFloat(coordmatch.group(7))) + " "+ str(coordmatch.group(8)).strip()
 
@@ -159,9 +155,9 @@ def findreferences(page, sqlfile):
 
             # finally...
             addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
-        
+
         return
-    
+
     latcompletematch  = latcompletepattern.search(pagetext)
     longcompletematch = longcompletepattern.search(pagetext)
 
@@ -171,7 +167,7 @@ def findreferences(page, sqlfile):
 
         coords = LatLon.string2latlon(latstring, longstring, 'd% %m% %S% %H')
         addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
-        
+
         return
 
     # try other patterns:
@@ -179,14 +175,14 @@ def findreferences(page, sqlfile):
     longdmatch = longdpattern.search(pagetext)
 
     if latdmatch and longdmatch:
-        
+
         # skip if the coordinates are empty:
         if len(latdmatch.group(1).strip()) == 0 or len(longdmatch.group(1).strip()) == 0:
             return
-        
+
         latd = float(latdmatch.group(1));
         longd = float(longdmatch.group(1));
-        
+
         # do we heave a heading?
         lathmatch = lathpattern.search(pagetext)
         longhmatch = longhpattern.search(pagetext)
@@ -205,16 +201,16 @@ def findreferences(page, sqlfile):
                 lonh = "W"
             else:
                 lonh = "E"
-        
+
         # if they are already decimals, we are done:
-        if isDecimal(latd) and isDecimal(longd):  
+        if isDecimal(latd) and isDecimal(longd):
             latstring  = str(latd)  + " " + lath
             longstring = str(longd) + " " + lonh
-            
+
             coords = LatLon.string2latlon(latstring, longstring, 'd% %H')
 
             # finally...
-            addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)          
+            addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
             return
 
         # do we have minutes?
@@ -229,21 +225,21 @@ def findreferences(page, sqlfile):
                 # print "Error trying to convert string to float - latm: " + str(latmmatch.group() + ", lonm: " + longmmatch.group()) # TODO - remove
                 latstring  = str(latd)  + " " + lath
                 longstring = str(longd) + " " + lonh
-                
+
                 coords = LatLon.string2latlon(latstring, longstring, 'd% %H')
 
                 # finally...
-                addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)          
+                addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
                 return
 
         else:
             latstring  = str(latd)  + " " + lath
             longstring = str(longd) + " " + lonh
-            
+
             coords = LatLon.string2latlon(latstring, longstring, 'd% %H')
 
             # finally...
-            addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)          
+            addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
             return
 
         # do we have seconds?
@@ -255,18 +251,14 @@ def findreferences(page, sqlfile):
         longsmatch = longspattern.search(pagetext)
 
         if(latsmatch and longsmatch):
-            try:
-                lats = float(latsmatch.group(1))
-                longs = float(longsmatch.group(1))
-            except Exception:
-                #logme("Error trying to convert string to float - lats: " + str(latsmatch.group() + ", lons: " + longsmatch.group())) # TODO - remove            
-                pass
-            
+            lats = float(latsmatch.group(1))
+            longs = float(longsmatch.group(1))
+
         #sqlfile.write(u"'" + str(int(latd)) + " " + str(latm) + " " + str(lats) + " " + lath + "', '" + str(int(longd)) + " " + str(longm) + " " + str(longs) + " " + lonh + "'\n")
 
         latstring = str(int(latd)) + " " + str(latm) + " " + str(lats) + " " + lath
         longstring = str(int(longd)) + " " + str(longm) + " " + str(longs) + " " + lonh
-        
+
         # just to check: sqlfile.write(latstring +" "+ longstring+ "\n")
 
         coords = LatLon.string2latlon(latstring, longstring, 'd% %m% %S% %H')
@@ -274,9 +266,9 @@ def findreferences(page, sqlfile):
         # finally...
         addInsert(pagetitle, coords.to_string('D')[0], coords.to_string('D')[1], sqlfile)
 
-                                     
+
 def go():
-    
+
     global logfile
 
     numthreads = int(sys.argv[1])
@@ -289,45 +281,41 @@ def go():
 
     logfile = open("logfile-"+str(thisthread)+"-of-"+str(numthreads)+".txt", "a")
 
-    # for the parsing, we follow the approach explained here: 
-    # http://www.ibm.com/developerworks/xml/library/x-hiperfparse/ 
+    # for the parsing, we follow the approach explained here:
+    # http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
     pages = etree.iterparse(infile, events=('end',), tag=ns+'page')
 
     count = 0
 
     # go through wikipedia pages in dump, one by one:
-    for event, page in pages:   
+    for event, page in pages:
         # print page title:
         # print page.find(ns+'title').text
-        count = count +1 
+        count = count +1
 
         # make sure we only parse every n-th page!
         if currentpage == thisthread:
-            try:
-                findreferences(page, sqlfile)
-                # free up some memory
-                page.clear()
-                # Also eliminate now-empty references from the root node to the page 
-                while page.getprevious() is not None:
-                    del page.getparent()[0]
-                
-            except Exception as e:
-                logme("Exception caught when parsing: " + page.find(ns+'title').text)
-                try:
-                    logme(str(e))
-                    # logme(page.find(ns+'revision/'+ns+'text').text)
-                except Exception:
-                    logme("Error could not be logged.")
-           
+            # try:
+            findreferences(page, sqlfile)
+            # free up some memory
+            page.clear()
+            # Also eliminate now-empty references from the root node to the page
+            while page.getprevious() is not None:
+                del page.getparent()[0]
+
+            # except Exception as e:
+            #     logme("Exception caught when parsing: " + page.find(ns+'title').text)
+            #     
+
         currentpage = currentpage + 1
         if currentpage > numthreads:
             currentpage = 1
-               
+
     # trigger housekeeping in DB after all inserts:
     insert = u"VACUUM;\n"
-    sqlfile.write(insert.encode('utf8')) 
+    sqlfile.write(insert.encode('utf8'))
 
-    sqlfile.close()  
+    sqlfile.close()
 
     print str(count) + " pages processed."
 

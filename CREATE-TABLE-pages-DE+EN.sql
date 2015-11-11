deae41e5160c0f48826fa108fdfe9ee44bd58c5f
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2015-11-11 15:00:27 -0500

    Wikidata parser also adds aliases now

--- CREATE-TABLE-pages-DE+EN.sql
+++ CREATE-TABLE-pages-DE+EN.sql
@@ -1,13 +1,20 @@
 DROP TABLE IF EXISTS pages;
 
 CREATE TABLE pages
-	(de 		varchar,
-	 en 		varchar,
-	 the_geom 	geometry,
-	 id 		serial primary key );
+	(de 					varchar,
+	 en 				  varchar,
+	 de_alias_for varchar,
+ 	 en_alias_for varchar,
+	 the_geom 		geometry,
+	 wikidata_id	varchar,
+	 id 					serial primary key );
 
 CREATE INDEX pages_geom_index ON pages USING GIST ( the_geom );
 
 CREATE INDEX page_name_index_de ON pages USING hash ( de );
 
 CREATE INDEX page_name_index_en ON pages USING hash ( en );
+
+CREATE INDEX page_alias_index_de ON pages USING hash ( de_alias_for );
+
+CREATE INDEX page_alias_index_en ON pages USING hash ( en_alias_for );
--- json2sql.js
+++ json2sql.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     JSONStream = require('JSONStream');
 
-var stream = fs.createReadStream('wikidata-20151102-all.json', {encoding: 'utf8'}),
+var stream = fs.createReadStream('/Users/carsten/wikidata-20151102-all.json', {encoding: 'utf8'}),
     parser = JSONStream.parse('*');
 
 function escapeSlashes( str ) {
@@ -11,16 +11,40 @@ function escapeSlashes( str ) {
 parser.on('data', function (obj) {
     // create insert statements for every wikidata item that has coordinates (property P625, see https://www.wikidata.org/wiki/Property:P625)
     if(obj.claims["P625"]){
+
         p625 = obj.claims["P625"];
         if(p625[0].mainsnak.datavalue){ // check whether this one actually has coordinates
+
             loc = p625[0].mainsnak.datavalue.value;
-            if(obj.labels["de"] && obj.labels["en"]){
-                console.log("INSERT INTO pages VALUES ('"+escapeSlashes(obj.labels["de"]["value"])+"', '"+escapeSlashes(obj.labels["en"]["value"])+"', ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326));")
-            } else if(obj.labels["de"]){ // in case there is only a english label
-                console.log("INSERT INTO pages VALUES ('"+escapeSlashes(obj.labels["de"]["value"])+"', '', ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326));")
-            } else  if(obj.labels["en"]){ // in case there is only a german label
-                console.log("INSERT INTO pages VALUES ('', '"+escapeSlashes(obj.labels["en"]["value"])+"',  ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326));")
-            } // skip if there is no english or german label
+
+            labelDE = '""';
+            labelEN = '""';
+
+            if(obj.labels["de"]){
+                labelDE = escapeSlashes(obj.labels["de"]["value"]);
+            }
+
+            if(obj.labels["en"]){
+                labelEN = escapeSlashes(obj.labels["en"]["value"]);
+            }
+
+            // skip if there is no english or german label
+            if((obj.labels["en"] !=  '""') || (obj.labels["en"] !=  '""')){
+                console.log("INSERT INTO pages VALUES ('"+labelDE+"', '"+labelEN+"', '', '', ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326), '"+obj.id+"');")
+
+                if(obj.aliases["de"]){
+                    for(var alias in obj.aliases["de"]){
+                        console.log("INSERT INTO pages VALUES ('"+escapeSlashes(obj.aliases["de"][alias]["value"])+"', '', '"+labelDE+"', '"+labelEN+"',  ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326), '"+obj.id+"');")
+                    }
+                }
+
+
+                if(obj.aliases["en"]){
+                    for(var alias in obj.aliases["en"]){
+                        console.log("INSERT INTO pages VALUES ( '', '"+escapeSlashes(obj.aliases["en"][alias]["value"])+"', '"+labelDE+"', '"+labelEN+"',  ST_SetSRID(ST_MakePoint("+loc.longitude+", "+loc.latitude+"),4326), '"+obj.id+"');")
+                    }
+                }
+            }
         }
     }
 });

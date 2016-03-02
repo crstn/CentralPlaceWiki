29fefae653eab287ba717a13651d2039dbcc4909
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-03-02 15:21:30 -0500

    Python script added to check completeness of DB.

---
+++ test_completeness.py
@@ -0,0 +1,27 @@
+import psycopg2, codecs
+
+# Try to connect
+try:
+    conn=psycopg2.connect("dbname='cpt'")
+except:
+    print "I am unable to connect to the database."
+
+cur = conn.cursor()
+
+f=codecs.open('DE_CPT_Hierarchy.txt', 'r', encoding='utf-8')
+
+for place in f.readlines():
+
+    place = place[:-1] # strip the \N from the end
+
+    #try:
+    cur.execute("""SELECT DISTINCT links.fromid, links."from", geo_tags.gt_name FROM links, geo_tags WHERE links.fromid = geo_tags.gt_page_id AND (  links."from" = %s );""", (place,))
+
+    # print cur.query
+
+    if cur.rowcount == 0:
+        print place.encode('utf-8') + " not found."
+
+    # rows = cur.fetchall()
+    # for row in rows:
+    #     print "   ", row

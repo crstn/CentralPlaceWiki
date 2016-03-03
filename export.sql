887c45800d5228b680a27790b5f7817faa7d03a0
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-03-03 17:11:13 -0500

    Formatting fixed

--- export.sql
+++ export.sql
@@ -1,2 +1,7 @@
-COPY geo_tags (gt_page_id, gt_lat, gt_lon, gt_name, gt_type, gt_country, gt_region) TO '/Users/carsten/Desktop/geo_tag.csv' NULL '\N' DELIMITER ',' CSV HEADER ESCAPE '\' ;
-COPY links TO '/Users/carsten/Desktop/links.csv' NULL '\N' DELIMITER ',' CSV HEADER ESCAPE '\' ;
+COPY geo_tags (gt_page_id, gt_lat, gt_lon, gt_name, gt_type, gt_country, gt_region)
+TO '/Users/carsten/Desktop/geo_tag.csv'
+NULL '\N' DELIMITER ',' CSV HEADER ESCAPE '\' ;
+
+COPY links
+TO '/Users/carsten/Desktop/links.csv'
+NULL '\N' DELIMITER ',' CSV HEADER ESCAPE '\' ;

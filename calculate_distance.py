829a3deae9d43a54f6eea8966dbcece14b33b00c
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-03-06 10:56:23 -0500

    There's something wrong with this one 😠

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from geopy.distance import great_circle
+from sqlalchemy import create_engine
 
 geotags = pd.read_csv('/Volumes/Solid Guy/CPT Python Data/geo_tag.csv', escapechar='\\', encoding='utf-8')
 
@@ -23,7 +24,7 @@ right.rename(columns={'fromid': 'toid', 'from': 'to'}, inplace=True)
 
 # left join to find the IDs of all 'linked-to' pages
 print 'Performing self-join'
-links = pd.merge(left=links, right=right, how='left', left_on='to', right_on='totitle')
+links = pd.merge(left=links, right=right, how='left', on='to')
 
 # left join to find the IDs of all 'linked-to' pages
 print 'Performing self-join'
@@ -33,10 +34,10 @@ links = pd.merge(left=links, right=right, how='left', left_on='to', right_on='to
 
 print 'Performing another left join to get the FROM lats and lons'
 
-geotags.rename(columns={'gt_lat': 'from_lat', 'gt_lon': 'from_lon'}, inplace=True)
-links = pd.merge(left=links, right=geotags, how='left', left_on='fromid', right_on='gt_page_id')
+geotags.rename(columns={'from_lat': 'to_lat', 'from_lon': 'to_lon', 'gt_name': 'to' }, inplace=True)
 
-links.drop(['gt_name', 'gt_type', 'gt_country', 'gt_region', 'to', 'gt_page_id'], axis=1, inplace=True)
+links = pd.merge(left=links, right=geotags, how='left', left_on='fromid', right_on='gt_page_id')
+links.drop(['gt_type', 'gt_country', 'gt_region', 'gt_page_id'], axis=1, inplace=True)
 
 print 'And another one to get the TO lats and lons'
 
@@ -57,9 +58,29 @@ pages = pd.merge(left=right, right=geotags, how='outer', on='gt_page_id')
 pages.rename(columns={'gt_page_id': 'page_id', 'to': 'page', 'to_lon': 'lon', 'to_lat': 'lat', 'gt_type': 'type', 'gt_country': 'country', 'gt_region': 'region'}, inplace=True)
 pages.drop(['gt_name'], axis=1, inplace=True)
 
+
+# we can write these dataframes to CSV files:
 print 'Exporting processed CSVs.'
 
-pages.to_csv('/Volumes/Solid Guy/CPT Python Data/pages_processed.csv', escapechar='\\', encoding='utf-8')
-links.to_csv('/Volumes/Solid Guy/CPT Python Data/links_processed.csv', escapechar='\\', encoding='utf-8')
+# make sure all integers are integers in the output:
+pages[['page_id']] = pages[['page_id']].astype(int)
+# the links are a bit trickier becaue the 'toid' column contains NaN values, which cannot be cast to int.
+# So we'll replace them with -1 here, and replace those with Null again in PostGIS later
+links[['toid']] = links[['toid']].fillna(-1.0).astype(int)
+
+# the pages dataframe did have one dublicates for me (no idea why), and this one didn't solve it:
+# pages.drop_duplicates()
+# so I had to go into the CSV and delete the line after export, otherwise it causes a hiccup in the DB import
+
+pages.to_csv('/Volumes/Solid Guy/CPT Python Data/pages_processed.csv', escapechar='\\', encoding='utf-8', index=False)
+links.to_csv('/Volumes/Solid Guy/CPT Python Data/links_processed.csv', escapechar='\\', encoding='utf-8', index=False)
+
+# ... or write them directly to the DB:
+
+# this requires some cleanup afterwards, so I did it the CSV way
+
+# engine = create_engine('postgresql://localhost:5432/cpt')
+# pages.to_sql('pages', engine)
+# links.to_sql('links', engine)
 
 print 'Good night'

6485db37d0b99347eff77decd13b8fb99b26547c
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-08-18 23:05:03 +0200

    Histogram with incoming references just from cities added

---
+++ Python Plots/Incoming from cities - all cities.sql
@@ -0,0 +1,9 @@
+﻿COPY (
+
+	SELECT p.page_id, p.page, p.incoming_from_cities
+	FROM pages p
+	WHERE p.type = 'city'
+
+	)
+
+TO '/Users/carsten/Dropbox/Code/CentralPlaceWiki/Python Plots/Incoming from cities - all cities.csv' WITH CSV HEADER;

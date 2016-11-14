2d2e8b6fe5a23896481587c62cd046afa31dd8fa
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-11-14 15:56:24 +0100

    Plotting reorganized

--- Python Plots/Incoming from cities - all cities.sql
+++
@@ -1,9 +0,0 @@
-﻿COPY (
-
-	SELECT p.page_id, p.page, p.incoming_from_cities
-	FROM pages p
-	WHERE p.type = 'city'
-
-	)
-
-TO '/Users/carsten/Dropbox/Code/CentralPlaceWiki/Python Plots/Incoming from cities - all cities.csv' WITH CSV HEADER;

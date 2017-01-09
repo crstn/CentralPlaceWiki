3084fb9da15be308050d74e450f951168eda96a2
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2017-01-09 13:59:35 +0100

    Added a SQL query that fetches the most-linked to upper center for every middle center

---
+++ MostLinkedUpperCenterPerMiddleCenter.sql
@@ -0,0 +1,8 @@
+﻿SELECT DISTINCT ON (l.from)  
+       l.from, l.to, l.mentions, l.linkid, l.line_geom
+FROM links l, centers_de c1, centers_de c2
+WHERE l.from = c1.city
+AND c1.type > 1
+AND l.to = c2.city
+AND c2.type = 1
+ORDER BY l.from, l.mentions DESC ;
